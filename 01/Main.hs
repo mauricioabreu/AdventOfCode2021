9811d75ca524cb85b23d289b563b127211a0bfbf
Maurício Antunes <mauricio.abreua@gmail.com>
2021-12-02 08:24:45 -0300

    Day 1

---
+++ 01/Main.hs
@@ -0,0 +1,22 @@
+import System.IO
+
+countIncreased :: [Int] -> Int
+countIncreased [] = 0
+countIncreased (x:y:xs)
+    | y > x = 1 + countIncreased (y:xs)
+    | otherwise = 0 + countIncreased (y:xs)
+countIncreased (x:xs) = 0
+
+sumOfWindows :: [Int] -> [Int]
+sumOfWindows [] = []
+sumOfWindows xs = sum (take 3 xs) : sumOfWindows (tail xs)
+
+main :: IO ()
+main = do
+        handle <- openFile "input" ReadMode
+        contents <- hGetContents handle
+        let lines = words contents
+        let list = map read lines
+        print (countIncreased list)
+        print (countIncreased $ sumOfWindows list)
+        hClose handle
